060dd99046b2410566978f22943957d624984dec
Arina Zyranova <zyranovaarina02@gmail.com>
2026-02-22 03:18:13 +0300

    прописала компонент header и стили для него

---
+++ floramind-app/src/components/layout/header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Container from '../ui/container'
+import './header.css'
+import {Link} from 'react-router-dom'
+
+const Header = () =>{
+    return (
+        <header className="header">
+            <Container>
+                <div className="nav-content">
+                    <img src="/images/first_screen/logo_firstscreen.png" alt="logo FloraMind" className="logo" />
+                    <nav className="nav">
+                         <Link to='/' className="header-link">О нас</Link>
+                        <Link to='/' className="header-link">Галерея</Link>
+                        <Link to='/' className="header-link">Отзывы</Link>
+                        <Link to='/' className="header-link">FloraAi</Link>
+                    </nav>
+                    <div className="auth_links">
+                        <Link to='/' className="header-link">Вход</Link>
+                        <p>|</p>
+                        <Link to='/' className="header-link">Регистрация</Link>
+                    </div>
+                </div>
+            </Container>
+        </header>
+    );
+
+}
+
+export default Header
